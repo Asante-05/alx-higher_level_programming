377282c42d7ef5d28ece492d03f2cb590d0bd93b
Asante Kelvin <kelvinowusu523@gmail.com>
2022-11-29 21:58:56 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+def getlast(number):
+	if(number < 0):
+		return number % -10
+	else:
+		return number % 10
+
+string1 = f"Last digit of {number} is {getlast(number)}"
+
+if getlast(number) > 5:
+	string2 =  "and is greater than 5"
+elif getlast(number) == 0:
+	string2 = "and is 0"
+elif getlast(number) < 5 and getlast(number) != 0:
+	string2 = "and is less than 6 and not 0"
+print(f"{string1} {string2}")
