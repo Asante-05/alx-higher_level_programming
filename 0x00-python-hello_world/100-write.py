f7909857aaedb49e6351163e500af62bc7057765
Asante Kelvin <kelvinowusu523@gmail.com>
2022-11-29 09:44:09 +0000

    asem oo

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-strig = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stderr.write(string)
+sys.stderr.wrtie( "and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
