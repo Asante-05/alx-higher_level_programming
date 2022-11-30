e3ec41415b5ca7357c98ef0749ab4db73c067c7f
Asante Kelvin <kelvinowusu523@gmail.com>
2022-11-30 01:10:38 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            string += chr(ord(char) - 32)
+            continue
+        string += char
+    print("{}".format(string))
