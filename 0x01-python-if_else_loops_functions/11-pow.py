6276e26c453d2ccbb7a21e4abfa38edaa25878e9
Asante Kelvin <kelvinowusu523@gmail.com>
2022-11-30 01:51:54 +0000

    python

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
     c = a
-    for i in range(1, b):
-        a *= c
-    return a
+    if b > 0:
+        for i in range(1, b):
+            a *= c
+        return a
+    else:
+        for i in range(1, -b):
+            a *= c
+        a = 1/a
+        return a
