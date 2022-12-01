17e817fa54ba4538a92495002c179bf92a86c718
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-01 18:57:01 +0000

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,16 +8,16 @@ if __name__ == "__main__":
 
     if (argv[2] == "+"):
         result = add(int(argv[1]), int(argv[3]))
-        print("{} + {} = {}".format(argv[1], argv[3], result))
+        print("{:d} + {:d} = {:d}".format(argv[1], argv[3], result))
     elif (argv[2] == "-"):
         result = sub(int(argv[1]), int(argv[3]))
-        print("{} - {} = {}".format(argv[1], argv[3], result))
+        print("{:d} - {:d} = {:d}".format(argv[1], argv[3], result))
     elif (argv[2] == "*"):
         retult = mul(int(argv[1]), int(argv[3]))
-        print("{} * {} = {}".format(argv[1], argv[3], result))
+        print("{:d} * {:d} = {:d}".format(argv[1], argv[3], result))
     elif (argv[2] == "/"):
         result = div(int(argv[1]), int(argv[3]))
-        print("{} / {} = {}".format(argv[1], argv[3], result))
+        print("{:d} / {:d} = {:d}".format(argv[1], argv[3], result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
