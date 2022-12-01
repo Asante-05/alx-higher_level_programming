3cc12d0996074353ea23ab9578a9ff13bb9ad97d
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-01 19:30:19 +0000

    python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def mex(a, b):
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
     if(a < b):
         c = add(a, b)
         for i in range(4, 6):
