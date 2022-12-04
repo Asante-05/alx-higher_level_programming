265120c8766a2782156e4ebe4fbafb47f852d45d
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-04 14:02:41 +0000

    python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "" or sentence is None:
-        return None
-    return len(sentence), sentence[0]
+    if sentence is not None:
+        if sentence:
+            return len(sentence), sentence[0]
+        return 0, None
+    return None, None
