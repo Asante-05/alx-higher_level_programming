d942ff35fa47bdc4baa4e4d1db42d897875e676f
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-20 10:13:25 +0000

    exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,8 @@
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-        return result
     except Exception as error:
         print("Execution: {}".format(error), file=sys.stderr)
-        return None
+        result = None
+    finally:
+        return result
