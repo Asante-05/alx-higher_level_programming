04a79a1fc0a878bcc95599c5b1a41ec5cebd9143
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-20 06:16:26 +0000

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,6 @@ def safe_print_list(my_list=[], x=0):
             c += 1
         except IndexError:
             raise IndexError
-    print()
-    return c
+        finally:
+            print()
+            return c
