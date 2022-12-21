0d8ddd16e600a165a993c303655d311252f3d952
Asante Kelvin <kelvinowusu523@gmail.com>
2022-12-21 00:44:29 +0000

    bytecode

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -53,4 +53,4 @@ class Square:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("{}".format("#"), end="")
-                print('\n')
+                print("")
