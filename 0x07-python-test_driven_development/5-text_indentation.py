a59048a7b063eae8e22c8a2e3544201d50a1c7d1
Asante Kelvin <kelvinowusu523@gmail.com>
2023-01-06 13:46:34 +0000

    task 0

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,17 +11,12 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    flag = 0
-    for i in text:
-        if flag == 0:
-            if i == ' ':
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if i in ['.', ':', '?']:
-                print(i)
-                print()
-                flag = 0
-            else:
-                print(i, end='')
+    string = text[:]
+
+    for c in ".?:":
+        list_text = string.strip(c)
+        string = ""
+        for i in list_text:
+            i = i.strip(" ")
+            string = i + c if string is "" else string + "\n\n" + i + c
+    print(string[:-3], end="")
