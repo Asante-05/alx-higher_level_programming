2a025790e5d9513b16628dad7f9881091fde51a4
Asante Kelvin <kelvinowusu523@gmail.com>
2023-01-09 21:38:27 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" empty class
+execute: ./6-main.py
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """ raise an exception """
+        raise Exception("area() is not implemented")    
