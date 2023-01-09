9df3d1c0cbeb68d945b6b9aa120634769f120605
Asante Kelvin <kelvinowusu523@gmail.com>
2023-01-09 23:37:29 +0000

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-""" Inherits from BasicGeometry
-execute: ./8-main.py
-"""
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+class Square(Rectangle):
+    """Represent a square"""
 
-
-class Rectangle(BaseGeometry):
-    """ class defining Rectange using basic geometry """
-
-    def __init__(self, width, height):
-        """ initializing a new rectange """
-
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """ returns the area of the rectangle """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ returns the print() and str() representation of a Rectangle """
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+    def __init__(self, size):
+        """Initialize a new square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
