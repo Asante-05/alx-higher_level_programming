b8993a899fe75f790f56aeaf866fc4c488d0d68b
Asante Kelvin <kelvinowusu523@gmail.com>
2023-01-11 00:27:56 +0000

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -9,5 +9,6 @@ def to_json_string(my_obj):
     """ functiont to encode json
     Args:
         myobj(object): json object
+    Return: json representation of object
     """
     return (json.dumps(my_obj))
