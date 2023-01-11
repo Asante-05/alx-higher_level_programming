cb34763ed7cd3dfae379dce84933e02eec384805
Asante Kelvin <kelvinowusu523@gmail.com>
2023-01-11 23:42:45 +0000

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,8 +3,8 @@
 
 
 def pascal_triangle(n):
-	"""Represents Pascal's Triangle of size n
-		"""
+    """Represents Pascal's Triangle of size n
+    """
     if n <= 0:
         return []
     triangles = [[1]]
