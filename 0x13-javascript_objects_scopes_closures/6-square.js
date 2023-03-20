62b5152d3cab39451168dfb88d6dc4ddb4f923cb
Asante Kelvin <kelvinowusu523@example.com>
2023-03-20 20:35:28 +0000

    more js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const Sq = require('./5-square');
+
+class Square extends Sq {
+	constructor (size) {
+		super(size, size);
+	}
+
+	charPrint (c) {
+		let output = '';
+		if (c === undefined) {
+			this.print();
+		} else {
+			for (let y = 0; y < this.height; y++) {
+				for (let x = 0; x < this.width; x++) {
+					output += c;
+				}
+				output += '\n';
+			}
+			output = output.substring(0, output.length - 1);
+			console.log(output);
+		}
+	}
+}
+
+module.exports = Square;
